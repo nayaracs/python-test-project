12d58720b79f395c89ad0a2887d1d6f2771d684c
nayaracs <nayaraslimac@gmail.com>
2021-01-23 17:28:01 -0300

    finalizado

--- src/nayaralima.py
+++ src/nayaralima.py
@@ -43,3 +43,9 @@ else:
     else:
         if R<L and R<B:
             print("Ridgewood")
+#Entrada1: Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)
+#Saída1: Lakewood
+#Entrada2: Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)
+#Saída2: Bridgewood
+#Entrada3: Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)
+#Saída3: Lakewood
